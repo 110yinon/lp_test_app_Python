33c171d0fd5a2aa4a0fd5348b1230811fd7505b2
yinon@litepoint <yinon@litepoint>
2023-03-20 17:27:28 +0200

    reading and parsing a config json file

--- demo.py
+++ demo.py
@@ -2,20 +2,25 @@ import DirCmdFramework
 import FilterFiles
 # from iqdvt.HelpResponse import HelpResponseFile
 from iqdvt.IqdvtCliActivator import IqdvtCliActivator
+import json
+# from config import configJSONFile
 
 # demo code:
 
+with open('./config/config.runner.json','r') as configJSONFile:
+    kuni = json.load(configJSONFile)
+    print(kuni)
 
 station='C:\\LitePoint\\stations\\celeno_16_02_2023.sta'
 flow='C:\\LitePoint\\flows\\txCal.flow'
 
-ExecutableFrameworkObj = IqdvtCliActivator(['--v1',f'--station={station}',f'--flow={flow}'])
+# ExecutableFrameworkObj = IqdvtCliActivator(['--v1',f'--station={station}',f'--flow={flow}'])
 # ExecutableFrameworkObj = IqdvtCliActivator(['--help'])
 # ExecutableFrameworkObj.printFlags()
 # ExecutableFrameworkObj.printMe()
 # ExecutableFrameworkObj.printAnalyzeFlags()
 # print(f'~~ contain help flag: {ExecutableFrameworkObj.ExecuteReturnOutput()}')
-print(f'~~ txCal.flow is pass: {ExecutableFrameworkObj.ExecuteReturnOutput()}')
+# print(f'~~ txCal.flow is pass: {ExecutableFrameworkObj.ExecuteReturnOutput()}')
 
 # ExecutableFrameworkObj = HelpResponseFile.HelpResponse()
 # ExecutableFrameworkObj.Execute()
